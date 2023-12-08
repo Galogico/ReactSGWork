9087101a1c1e38a7040f82a0d09963f55a157aa1
galogico <galogico@gmail.com>
2023-12-08 15:37:52 -0300

    adicionando o soponsored e o about us

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from './components/navbar.jsx';
 import "./App.css"
 import About from './components/about.jsx';
+import Sponsored from './components/sponsored.jsx';
+import Aboutus from "./components/aboutus.jsx"
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
+      <Sponsored/>
+      <Aboutus/>
     </div>
   );
 }
---
+++ src/components/aboutus.jsx
@@ -0,0 +1,15 @@
+import './Aboutus.css'
+
+const Aboutus = () =>{
+    return(
+        <div className='general'>
+            <h1 className='txt title'>About Us</h1>
+            <p className='txt complementar'><span>Lorem, ipsum dolor sit amet consectetur<br/> adipisicing elit.</span></p>
+            <div>
+                <h1 className='txt titu2'>Get Popular NFT</h1>
+                <p className='txt'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/> Etiam eu turpis molestie, dictum est a, mattis tellus. Sed<br/> dignissim, metus nec fringilla accumsan, risus sem<br/> sollicitudin lacus, ut interdum tellus elit sed risus.<br/> Maecenas eget condimentum velit, sit amet feugiat<br/> lectus. Class aptent taciti sociosqu ad litora torquent per<br/> conubia nostra, per inceptos himenaeos. Praesent auctor<br/> purus luctus enim egestas, ac scelerisque ante pulvinar. </p>
+            </div>
+        </div>
+    )
+}
+export default Aboutus
